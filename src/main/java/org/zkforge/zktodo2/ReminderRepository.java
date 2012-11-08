4a176d25563b5d58c78358c57cdeeef06aa68114
Alexander Orlov <alexander.orlov@loxal.net>
2012-11-08 22:25:49 +0100

    * syntax error fix

--- src/main/java/org/zkforge/zktodo2/ReminderRepository.java
+++ src/main/java/org/zkforge/zktodo2/ReminderRepository.java
@@ -4,18 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 /**
- * Here we use spring-data for all the helper logic of working with queries. We 
- * don't provide an implementation of this class as the runtime behaviour is 
- * all provided by spring-data-jpa. 
- * 
+ * Here we use spring-data for all the helper logic of working with queries. We
+ * don't provide an implementation of this class as the runtime behaviour is
+ * all provided by spring-data-jpa.
+ * <p/>
  * We should really use a PagingAndSortingRepository but to keep the example simple
- * we will not. 
- * 
- * {@link http://gordondickens.com/wordpress/2011/08/02/adding-queries-to-spring-data-jpa/}
- * @author simbo
+ * we will not.
+ * <p/>
+ * http://gordondickens.com/wordpress/2011/08/02/adding-queries-to-spring-data-jpa/
  *
+ * @author simbo
  */
 public interface ReminderRepository extends JpaRepository<Reminder, Long> {
-	@Query("from Reminder where name = ?1")
-	Reminder findByName(String name);
+  @Query(value = "select r from Reminder r where r.name= ?1")
+  Reminder findByName(String name);
 }
