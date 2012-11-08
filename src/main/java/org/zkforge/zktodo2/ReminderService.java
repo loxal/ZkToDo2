dad104c16f0d8f3378fbaca2c8976be84698bfc3
Alexander Orlov <alexander.orlov@loxal.net>
2012-11-08 22:15:03 +0100

    * CloudFoundry update

--- src/main/java/org/zkforge/zktodo2/ReminderService.java
+++ src/main/java/org/zkforge/zktodo2/ReminderService.java
@@ -1,35 +1,33 @@
 package org.zkforge.zktodo2;
 
-import java.util.List;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.List;
 
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 /**
- * A trivial transactional business service. In a real app a service may use 
- * many repositories (DAOs) and may use many helper services.  
+ * A trivial transactional business service. In a real app a service may use
+ * many repositories (DAOs) and may use many helper services.
  */
 @Named("reminderService")
 public class ReminderService {
-	@Inject
-	protected ReminderRepository reminderRepository;
+  @Inject
+  protected ReminderRepository reminderRepository;
 
-	@Transactional(readOnly=true)
-	public List<Reminder> findAll(){
-		return this.reminderRepository.findAll();
-	}
-	
-	@Transactional(readOnly=false,propagation = Propagation.REQUIRED)
-	public void persist(Reminder reminder){
-		this.reminderRepository.save(reminder);
-	}
+  @Transactional(readOnly = true)
+  public List<Reminder> findAll() {
+    return this.reminderRepository.findAll();
+  }
 
-	@Transactional(readOnly=false,propagation = Propagation.REQUIRED)
-	public void delete(Reminder reminder)  {
-		this.reminderRepository.delete(reminder);
-	}
+  @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+  public void persist(Reminder reminder) {
+    this.reminderRepository.save(reminder);
+  }
 
-
+  @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+  public void delete(Reminder reminder) {
+    this.reminderRepository.delete(reminder);
+  }
 }
